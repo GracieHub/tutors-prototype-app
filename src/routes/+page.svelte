d6e539920c61ad13bedd6eda74564e534b17c07e
Grace Doyle <graceydoyle@gmail.com>
2023-02-25 13:15:40 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { child, get, getDatabase, onValue, ref, off } from "firebase/database";
+
+let statusRef = ref(getDatabase(),'StudentInfo');
+	onValue(statusRef, async () => {
+		console.log("student info received");
+	});
+</script>
+
+<div class="container mx-auto p-8 space-y-8">
+	<h1>Tutors Live Prototype</h1>
+	<p>Students Info </p>
+
+	<section class="flex space-x-2">
+		<div class="flex justify-center" />
+
+	</section>
+</div>
