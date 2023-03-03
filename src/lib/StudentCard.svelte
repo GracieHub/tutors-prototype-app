031c498c65b6c50c965efed6426635f8819c1a56
Grace Doyle <graceydoyle@gmail.com>
2023-03-03 09:27:11 +0000

    create StudentCard and call from main page

---
+++ src/lib/StudentCard.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import { Avatar } from '@skeletonlabs/skeleton';
+    import FaPhone from 'svelte-icons/fa/FaPhone.svelte';
+    import FaFacebookMessenger from 'svelte-icons/fa/FaFacebookMessenger.svelte';
+    export let courseEvent: any;
+</script>
+<div class="card w-60 h-[24rem] overflow-x-hidden m-1 border-y-8 border-primary-500">
+    <div class="flex">
+        <header class="card-header inline-flex items-center">
+            <div class="inline-flex w-full">
+                <Avatar src="/student1.jpg" alt={courseEvent.name} class="mr-2" />
+                <h6>
+                    {courseEvent.name}
+                    <div>
+                        <button
+                            class="btn variant-filled-secondary btn-base ring-2 ring-primary-500 ring-inset text-filled-500"
+                        >
+                            <div class="icon" style="color: orange; width: 20px; height:16px">
+                                <FaFacebookMessenger />
+                            </div>
+                        </button>
+                        <button
+                            class="btn variant-filled-secondary btn-base ring-2 ring-primary-500 ring-inset text-filled-500"
+                        >
+                            <div class="icon" style="color: green; width: 20px; height:16px">
+                                <FaPhone />
+                            </div>
+                        </button>
+                    </div>
+                </h6>
+            </div>
+        </header>
+    </div>
+    <figure class="flex justify-center object-scale-down mt-4 p-1 h-40">
+        <img src="/topic.jpg" alt="topic Image here" />
+    </figure>
+    <div class="card-body text-center">
+        Studying Lab: {@html courseEvent.course}
+    </div>
+    <footer class="card-footer text-center">
+        <p class="mt-2 italic">
+            {@html courseEvent.topic}
+        </p>
+    </footer>
+</div>
