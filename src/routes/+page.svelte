2967cc58e3f46b2d3011a6e207a05ed4cb2d2caa
Grace Doyle <graceydoyle@gmail.com>
2023-03-07 10:41:16 +0000

    If student name already exits, update card, if not, create new card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,21 +3,37 @@
     import { child, get, getDatabase, onValue, ref, off } from 'firebase/database';
     import { onMount } from 'svelte';
     import { getKeys } from '../environment';
-    import Card from '$lib/StudentCard.svelte';
+    import StudentCard from '$lib/StudentCard.svelte';
     /**
      * @type {any[]}
      */
     let courseEvents = [];
+    // A Map of all students seen so far (name) ==> Student Data)
+    let studentRecords = new Map();
     onMount(async () => {
         initFirebase(getKeys().firebase);
         let statusRef = ref(getDatabase(), 'StudentInfo');
         onValue(statusRef, async (snapshot) => {
-            console.log('student info received');
             if (snapshot) {
-                const data = snapshot.val();
-                courseEvents.push(data);
+                // a new event has been seen
+                const studentEvent = snapshot.val();
+                console.log('student info received');
+                console.log(studentEvent);
+                // Have we seen this student before?
+                const studentRecord = studentRecords.get(studentEvent.name);
+                if (!studentRecord) {
+                    // Nope, this is a new student. Lets store the student in our map
+                    studentRecords.set(studentEvent.name, studentEvent);
+                    // .. and lets push onto the aray of live student cards we are displaying
+                    courseEvents.push(studentEvent);
+                } else {
+                    // we have already seen this student, so lets update the course info...
+                    studentRecord.course = studentEvent.course;
+                    // .. and lets also update the topic
+                    studentRecord.topic = studentEvent.topic;
+                }
+                // touch all sutdent records, so that if any have changed they will be refreshed in screen
                 courseEvents = [...courseEvents];
-                console.log(data);
             }
         });
     });
@@ -28,7 +44,7 @@
     <section class="flex space-x-2">
         <div class="flex justify-center" />
         {#each courseEvents as courseEvent}
-            <Card {courseEvent} />
+            <StudentCard {courseEvent} />
         {/each}
     </section>
 </div>
